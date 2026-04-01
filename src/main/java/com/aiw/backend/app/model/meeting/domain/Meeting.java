6ad1f5201909c93108cc34fa61aa8556e834e062
yoozafree <tjdbs929@gmail.com>
2026-04-02 04:55:20 +0900

    ✨ feat: 회의 api

--- src/main/java/com/aiw/backend/app/model/meeting/domain/Meeting.java
+++ src/main/java/com/aiw/backend/app/model/meeting/domain/Meeting.java
@@ -1,11 +1,15 @@
 package com.aiw.backend.app.model.meeting.domain;
 
+import com.aiw.backend.app.model.project.domain.Project;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -48,6 +52,10 @@ public class Meeting {
 
   @Column(nullable = false, columnDefinition = "tinyint", length = 1)
   private Boolean activated;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "project_id", nullable = false)
+  private Project project;
 
   @CreatedDate
   @Column(nullable = false, updatable = false)
